60f43dba917fd92c3c27271ce09ffb9f23322677
yassine rakcho <zerox3203@gmail.com>
2023-09-21 10:31:40 +0100

    rectification

--- les tableaux/challenge7/challenge7.c
+++ les tableaux/challenge7/challenge7.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    int L, C; // Dimensions du tableau
-    int T[50][50]; // Tableau à deux dimensions
-    int somme = 0; // Variable pour stocker la somme des éléments
+    int L, C; 
+    int T[50][50]; 
+    int somme = 0; 
 
-    // Demande à l'utilisateur de saisir les dimensions L et C
+   
     printf("Entrez le nombre de lignes (max 50) : ");
     scanf("%d", &L);
     printf("Entrez le nombre de colonnes (max 50) : ");
     scanf("%d", &C);
 
-    // Vérifie que les dimensions sont valides
+    
     if (L <= 0 || L > 50 || C <= 0 || C > 50) {
-        printf("Les dimensions entrées ne sont pas valides.\n");
-        return 1; // Quitte le programme avec une erreur
+        printf("Les dimensions entrï¿½es ne sont pas valides.\n");
+        return 1; 
     }
 
-    // Remplit le tableau avec des valeurs entrées au clavier
-    printf("Saisissez les éléments du tableau :\n");
+    
+    printf("Saisissez les ï¿½lï¿½ments du tableau :\n");
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < C; j++) {
             printf("T[%d][%d] : ", i, j);
             scanf("%d", &T[i][j]);
-            somme += T[i][j]; // Ajoute la valeur à la somme
+            somme += T[i][j]; 
         }
     }
 
-    // Affiche le tableau
+    
     printf("Tableau :\n");
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < C; j++) {
-            printf("%d ", T[i][j]); // Affiche chaque élément avec un espace de 4 caractères
+            printf("%d ", T[i][j]); 
         }
-        printf("\n"); // Passe à la ligne suivante
+        printf("\n"); 
     }
 
-    // Affiche la somme des éléments
-    printf("Somme de tous les éléments : %d\n", somme);
+    
+    printf("Somme de tous les ï¿½lï¿½ments : %d\n", somme);
 
     return 0;
 }
